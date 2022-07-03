ba30bd88b8aec3881b21097a4d857e0d8ebce73b
azizfath <34805268+azizfath@users.noreply.github.com>
2022-07-03 21:03:54 +0700

    fixing home

--- main/assets/js/index.js
+++ main/assets/js/index.js
@@ -1,29 +1,23 @@
-let promoNodeText = ""
-let promoIndicatorNode = ""
+// let promoIndicatorNode = ""
 
-let promo = [{
-    filename: "promo_1.png"
-}, {
-    filename: "promo_2.png"
-}]
+// let promo = [{
+//     filename: "promo_1.png"
+// }, {
+//     filename: "promo_2.png"
+// }]
 
-promo.forEach((item, index) => {
-    promoNodeText =
-        "<div class=\"carousel-item\">" +
-        "<img src=\"./assets/images/promo/" + item.filename + "\" class=\"d-block w-100\">" +
-        "</div>"
+// promo.forEach((item, index) => {
 
-    promoIndicatorNode =
-        "<button " +
-        "type=\"button\" " +
-        "data-bs-target=\"#carousel-promo\" " +
-        "data-bs-slide-to=\"" + (index + 1) + "\" " +
-        "aria-label=\"Slide " + (index + 2) + "\" " +
-        "></button>"
+//     promoIndicatorNode =
+//         "<button " +
+//         "type=\"button\" " +
+//         "data-bs-target=\"#carousel-promo\" " +
+//         "data-bs-slide-to=\"" + (index + 1) + "\" " +
+//         "aria-label=\"Slide " + (index + 2) + "\" " +
+//         "></button>"
 
-    document.querySelector("#promo-indicators").innerHTML += promoIndicatorNode
-        // document.querySelector("#promo-inner").innerHTML += promoNodeText
-})
+//     document.querySelector("#promo-indicators").innerHTML += promoIndicatorNode
+// })
 
 let carouselProduct = document.querySelector("#carousel-product")
 let carouselInner = document.querySelectorAll('.carousel-inner-product')[0]
@@ -34,7 +28,6 @@ if (window.matchMedia("(min-width: 768px)").matches) {
     let scrollPosition = 0
 
     document.getElementById("next-btn").addEventListener("click", () => {
-        console.log("haha")
         if (scrollPosition < carouselWidth - cardWidth * 4) {
             scrollPosition += cardWidth
             carouselInner.scroll({
