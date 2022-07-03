0596346b2c79d920a181f871533a0097e949dd53
bt66 <bastian.bt66@gmail.com>
2022-07-03 08:00:56 +0700

    add admin page

---
+++ routes/admin_router.js
@@ -0,0 +1,42 @@
+var express = require('express');
+const axios = require('axios')
+var router = express.Router();
+
+router.get('/',async (req, res, next) => {
+    result = await axios.get('https://api.fiberku.site/aboutMeView')
+    res.render('pages/admin/index', {data: result.data});
+})
+
+//  frontend content setting
+router.get('aboutMeSetting',(req, res, next) => {
+
+})
+
+router.get('homeSetting',(req, res, next) => {
+    
+})
+
+router.get('contact',(req, res, next) => {
+    
+})
+
+//  product setting
+router.get('product',(req, res, next) => {
+    
+})
+
+router.get('mediaTransmission',(req, res, next) => {
+    
+})
+
+// team setting
+router.get('task',(req, res, next) => {
+    
+})
+
+// 
+router.get('team',(req, res, next) => {
+    
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3001
 var path = require('path')
+var adminRouter = require('./routes/admin_router');
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '/assets')))
 app.use('/assets', express.static(path.join(__dirname, '/assets')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/admin', adminRouter)
 
 app.get('/', function(req, res) {
     res.render('pages/index')
