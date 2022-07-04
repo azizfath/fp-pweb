ffde548c8de0ef89bc51c6d1c60d7491cc38c864
azizfath <34805268+azizfath@users.noreply.github.com>
2022-07-04 19:39:00 +0700

    removing some files

--- admin2/assets/js/admin.js
+++
@@ -1,9 +0,0 @@
-const toogleNavbar = document.getElementById('toogleNavbar');
-const sidebar = document.getElementsByClassName('sidebar');
-const sidebarHeader = document.getElementsByClassName('side-header');
-
-toogleNavbar.addEventListener('click', ()=> {
-    sidebar[0].classList.toggle('hidden');
-    console.log(window.innerWidth)
-    console.log('clicked');
-})
