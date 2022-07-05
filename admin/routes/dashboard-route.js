40b3078730f4cc468935ffaf7773904b7540663f
azizfath <34805268+azizfath@users.noreply.github.com>
2022-07-05 08:39:59 +0700

    removing admin

--- admin/routes/dashboard-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/dashboard', checkAuth, async(req, res) => {
-    await res.render('pages/dashboard', { name: req.user.name, pass: req.user.pass })
-})
-
-module.exports = router;
